c80704b3d65ab6d55282bf9152d49ab59960b607
Taras Ishchuk <ishchuktaras@gmail.com>
2022-12-27 14:59:00 +0100

    create-react-app lesson

---
+++ 05-react-jsx-green-button/script.js
@@ -0,0 +1,18 @@
+
+const App = ({ initialButtonText, initialClsssesList }) => {
+  
+  const [buttonText, setButtonText] = React.useState(initialButtonText)
+  const [classesList, setClassesList] = React.useState(initialClsssesList)
+  const onButtonClick = () => {
+    setButtonText ('Hello from React')
+    setClassesList('green-btn')
+  }
+  return (
+    <div className="app">
+      <button className={classesList} onClick={onButtonClick}>{buttonText}</button>
+    </div>
+  )
+}
+const container = document.getElementById("app")
+const root = ReactDOM.createRoot(container)
+root.render(<App initialButtonText="Click me" initialClsssesList="blue-btn"/>)
